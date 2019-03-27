f721cf3de22761a5011d8aca86df448a162c28ce
JenT83 <43851846+JenT83@users.noreply.github.com>
2019-03-26 21:32:57 -0400

    flask outline

---
+++ app.py
@@ -0,0 +1,69 @@
+import os
+
+import pandas as pd
+import numpy as np
+import datetime as dt 
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+#################################################
+# Database Setup
+#################################################
+
+# need to make sure that the reference is updated for the structure
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/natality.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# need to make sure that the reference is updated for the structure
+# Save references to each table
+state = Base.classes.state
+national = Base.classes.national
+
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+@app.route("/data")
+def data():
+    """Return the state level data"""
+    return render_template("data.html")
+
+
+@app.route("/nationalchoropleth")
+def n_choropleth(national):
+    """Return the choropleth for US"""
+    return render_template("national.html")
+
+
+
+@app.route("/statechoropleth")
+def s_choropleth(state):
+    """Return the choropleth for the state"""
+    return render_template("state.html")
+
+
+@app.route("/charts")
+def s_choropleth():
+    """Return the charts"""
+    return render_template("charts.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
